fa22258a6c9818723884e4807ee1f3e6f2ebb39d
Saman <saman@foodbomb.com.au>
2023-03-01 21:31:16 +0800

    set up typescript express environment, add s3 initial config, it need more work

---
+++ backend/dist/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import AWS from "aws-sdk";
+import fs from "fs";
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+const app = express();
+// Health check
+app.get("/", (req, res) => {
+    res.send({ message: "App for image uploader challenge" });
+});
+// Make a call to aws s3
+app.post("/image-upload", (req, res) => {
+    const imagePath = req.body.imagePath;
+    const blob = fs.readFileSync(imagePath);
+    const uploadedImage = s3
+        .upload({
+        // I need to fix this issue
+        // @ts-ignore
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: "image.jpg",
+        Body: blob,
+    })
+        .promise();
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`app is listen on port ${8080}`);
+});
